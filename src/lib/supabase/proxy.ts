2170a0dcd61981a92be4f33b0ec755ef15524a23
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-04-13 03:41:01 +0300

    Update proxy.ts

--- src/lib/supabase/proxy.ts
+++ src/lib/supabase/proxy.ts
@@ -40,12 +40,7 @@ export async function updateSession(request: NextRequest) {
   const { data } = await supabase.auth.getClaims()
   const user = data?.claims
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/register') &&
-    !request.nextUrl.pathname.startsWith('/auth')
-  ) {
+  if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/login'
