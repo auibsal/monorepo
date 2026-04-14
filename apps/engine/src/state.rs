9ed6e07aaea2e9fed7797c2c1026234efe89421b
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-04-14 07:13:39 +0300

    Update state.rs

--- apps/engine/src/state.rs
+++ apps/engine/src/state.rs
@@ -1,4 +1,4 @@
-use crate::bitboard::{Domino, TileSet};
+use crate::bitboard::{Domino, TileSet, Action}; // <-- Import Action
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct GameState {
@@ -14,7 +14,7 @@ impl GameState {
     pub fn new() -> Self {
         Self {
             hands: [TileSet::empty(); 4],
-            unplayed_tiles: TileSet(0x0FFFFFFF), // 28 bits set to 1
+            unplayed_tiles: TileSet(0x0FFFFFFF),
             open_ends: None,
             current_turn: 0,
             passed_players: [0; 4],
@@ -22,11 +22,11 @@ impl GameState {
         }
     }
 
-    pub fn generate_legal_moves(&self) -> Vec<Domino> {
-        let mut moves = Vec::with_capacity(7);
+    // Now returns Vec<Action>
+    pub fn generate_legal_moves(&self) -> Vec<Action> {
+        let mut actions = Vec::with_capacity(14); // Max 7 tiles * 2 ends
         let my_hand = &self.hands[self.current_turn];
 
-        // Decode the Bitboard to find out what tiles are actually in the hand
         let mut all_hand_tiles = Vec::new();
         for high in 0..=6 {
             for low in 0..=high {
@@ -38,34 +38,43 @@ impl GameState {
         }
 
         if let Some((end1, end2)) = self.open_ends {
-            // The board is active. Filter tiles that match the open ends.
             for d in all_hand_tiles {
-                if d.high == end1 || d.low == end1 || d.high == end2 || d.low == end2 {
-                    moves.push(d);
+                // Can we play it on the first end?
+                if d.high == end1 || d.low == end1 {
+                    actions.push(Action { tile: d, target_pip: Some(end1) });
+                }
+                // Can we play it on the second end? (Avoid duplicates if ends are identical)
+                if end1 != end2 && (d.high == end2 || d.low == end2) {
+                    actions.push(Action { tile: d, target_pip: Some(end2) });
                 }
             }
         } else {
-            // First turn of the round. Any tile in hand can be played.
-            moves = all_hand_tiles;
+            // First turn of the round
+            for d in all_hand_tiles {
+                actions.push(Action { tile: d, target_pip: None });
+            }
         }
 
-        moves
+        actions
     }
 
-    pub fn apply_move(&mut self, domino: Domino, played_on_end: u8) {
-        self.hands[self.current_turn].remove(&domino);
-        self.unplayed_tiles.remove(&domino);
+    // Now accepts an Action instead of (Domino, u8)
+    pub fn apply_move(&mut self, action: Action) {
+        self.hands[self.current_turn].remove(&action.tile);
+        self.unplayed_tiles.remove(&action.tile);
         self.consecutive_passes = 0;
 
         if let Some((e1, e2)) = self.open_ends {
-            let other_half = if domino.high == played_on_end { domino.low } else { domino.high };
+            let played_on_end = action.target_pip.unwrap();
+            let other_half = if action.tile.high == played_on_end { action.tile.low } else { action.tile.high };
+            
             if e1 == played_on_end {
                 self.open_ends = Some((other_half, e2));
             } else {
                 self.open_ends = Some((e1, other_half));
             }
         } else {
-            self.open_ends = Some((domino.high, domino.low));
+            self.open_ends = Some((action.tile.high, action.tile.low));
         }
 
         self.current_turn = (self.current_turn + 1) % 4;
