9c0d3883536dd413f74f9345ecc5cc562161cda4
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-04-25 23:55:58 +0300

    Update page.tsx

--- apps/web/src/app/[locale]/bylaws/page.tsx
+++ apps/web/src/app/[locale]/bylaws/page.tsx
@@ -1,6 +1,6 @@
 import { getLocale } from 'next-intl/server';
-import BylawsEN from 'en';
-import BylawsAR from 'ar';
+import BylawsEN from 'EN';
+import BylawsAR from 'AR';
 
 // This is a Server Component. It calculates the locale instantly on the server
 // and only sends the correct language component to the browser, saving bandwidth.
