abbb64b118276d4d63de2f6a08f0fb3e961623d6
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-04-28 08:00:45 +0300

    Update server.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- packages/supabase/src/server.ts
+++ packages/supabase/src/server.ts
@@ -1,10 +1,11 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import { Database } from './types';
 
 export const createClient = () => {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
@@ -15,16 +16,12 @@ export const createClient = () => {
         set(name: string, value: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // Handle cookie setting in Server Components
-          }
+          } catch (error) {}
         },
         remove(name: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value: '', ...options });
-          } catch (error) {
-            // Handle cookie removal in Server Components
-          }
+          } catch (error) {}
         },
       },
     }
