d17cf31a53d992e870603bedf4ef3c7e4eaa238d
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-04-28 09:43:12 +0300

    Update middleware.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- packages/supabase/src/middleware.ts
+++ packages/supabase/src/middleware.ts
@@ -1,11 +1,10 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export const updateSession = async (request: NextRequest) => {
-  let response = NextResponse.next({
-    request: { headers: request.headers },
-  });
-
+export const updateSession = async (
+  request: NextRequest, 
+  response: NextResponse = NextResponse.next({ request: { headers: request.headers } })
+) => {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -14,12 +13,10 @@ export const updateSession = async (request: NextRequest) => {
         get(name: string) { return request.cookies.get(name)?.value; },
         set(name: string, value: string, options: CookieOptions) {
           request.cookies.set({ name, value, ...options });
-          response = NextResponse.next({ request: { headers: request.headers } });
           response.cookies.set({ name, value, ...options });
         },
         remove(name: string, options: CookieOptions) {
           request.cookies.set({ name, value: '', ...options });
-          response = NextResponse.next({ request: { headers: request.headers } });
           response.cookies.set({ name, value: '', ...options });
         },
       },
