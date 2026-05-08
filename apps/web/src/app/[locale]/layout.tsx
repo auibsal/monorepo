2ff89cb643f4a54732d43560a7a9f9aaa557e00f
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-08 15:14:14 +0300

    Update layout.tsx
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -2,8 +2,8 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { Ubuntu } from 'next/font/google';
 import localFont from 'next/font/local'; // Import the local loader
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import Navbar from '@/components/layout/Navbar';
+import Footer from '@/components/layout/Footer';
 import '../globals.css';
 
 // 1. Keep Google Fonts for the English (Latin) version
