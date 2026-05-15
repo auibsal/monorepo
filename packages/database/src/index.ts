9c6015f587db954fddbedde4f5e3ec63b6a928df
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-16 02:37:49 +0300

    Update index.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,8 +1,1 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js';
-import { Database } from './schema';
-
-export const createClient = (supabaseUrl: string, supabaseKey: string) => {
-  return createSupabaseClient<Database>(supabaseUrl, supabaseKey);
-};
-
 export * from './schema';
