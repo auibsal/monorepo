d4685cc193a5018b563de98346c104f5549c6bb7
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-15 15:21:07 +0300

    Update client.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,6 +1,10 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-export function createClient(supabaseUrl: string, supabaseAnonKey: string) {
+export function createClient() {
+  // We use the non-null assertion operator (!) to fail loudly if keys are missing
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
   return createBrowserClient(
     supabaseUrl,
     supabaseAnonKey
