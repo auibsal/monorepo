ef62503d3461a41e65ecab684831b118b5abd4c0
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-16 03:07:02 +0300

    Update index.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,6 @@
 export * from './lib/utils';
 export * from './components/ui/button';
 export { default as Logo } from './components/Logo';
-export * from './fonts';
 export { default as Navbar } from './components/layout/Navbar';
 export type { NavbarProps, NavbarLink } from './components/layout/Navbar';
 export * from './components/RichTextEditor';
