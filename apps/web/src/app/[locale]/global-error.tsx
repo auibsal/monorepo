3d6c3490957058228ccd0133cb8fc0b595e23783
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-19 03:23:19 +0300

    Create global-error.tsx
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

---
+++ apps/web/src/app/[locale]/global-error.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { InteractiveErrorState } from '@auibsal/ui';
+import '../globals.css';
+
+export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
+  return (
+    <html lang="en">
+      <body>
+        <InteractiveErrorState 
+          code="SYS_CRITICAL"
+          title="Total Collapse"
+          message="The fundamental architecture of this environment has failed to initialize."
+          actionText="Force Reboot"
+          onAction={() => reset()}
+          isRtl={false}
+        />
+      </body>
+    </html>
+  );
+}
