d0166bc15e47fa680e9cc570eb3f834f9daa298e
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-20 18:07:36 +0300

    Update index.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -9,3 +9,6 @@ export type Tables<T extends keyof Database['public']['Tables']> = Database['pub
 export type InsertTables<T extends keyof Database['public']['Tables']> = Database['public']['Tables'][T]['Insert'];
 export type UpdateTables<T extends keyof Database['public']['Tables']> = Database['public']['Tables'][T]['Update'];
 export type Enums<T extends keyof Database['public']['Enums']> = Database['public']['Enums'][T];
+
+// 3. Runtime Constants & UI Mappings
+export * from './constants';
