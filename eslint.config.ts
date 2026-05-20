533a8914583b4b65d80b85530ca153ea8fb99008
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-20 05:07:54 +0300

    Update and rename eslint.config.mjs to eslint.config.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

---
+++ eslint.config.ts
@@ -0,0 +1,12 @@
+import { baseConfig } from "@auibsal/config/eslint.config";
+import type { Linter } from "eslint";
+
+const config: Linter.Config[] = [
+  ...baseConfig,
+  {
+    // CRITICAL: Stop the root from double-linting the workspaces
+    ignores: ["apps/**", "packages/**"], 
+  },
+];
+
+export default config;
