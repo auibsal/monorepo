9b9ab2e660103e43030fff87f9c19e5f2a2c8518
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-20 19:22:41 +0300

    Update index.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- apps/web/src/fonts/index.ts
+++ apps/web/src/fonts/index.ts
@@ -1,24 +1,26 @@
- import { Ubuntu } from 'next/font/google';
-   import localFont from 'next/font/local';
+import { Ubuntu } from 'next/font/google';
+import localFont from 'next/font/local';
 
-   export const ubuntu = Ubuntu({
-     weight: ['300', '400', '500', '700'],
-     subsets: ['latin'],
-     variable: '--font-ubuntu'
-   });
+export const ubuntu = Ubuntu({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  variable: '--font-ubuntu',
+  display: 'swap',
+});
 
-   export const ubuntuArabic = localFont({
-     src: [
-       {
-         path: './UbuntuArabic-Regular.ttf',
-         weight: '400',
-         style: 'normal',
-       },
-       {
-         path: './UbuntuArabic-Bold.ttf',
-         weight: '700',
-         style: 'normal',
-       }
-     ],
-     variable: '--font-ubuntu-arabic'
-   });
+export const ubuntuArabic = localFont({
+  src: [
+    {
+      path: './UbuntuArabic-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './UbuntuArabic-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    }
+  ],
+  variable: '--font-ubuntu-arabic',
+  display: 'swap',
+});
