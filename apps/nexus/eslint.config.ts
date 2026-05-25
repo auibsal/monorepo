852cf5ed772983e95a4f8181d740de7f3c8963c3
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-25 09:55:02 +0300

    Update eslint.config.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

--- apps/nexus/eslint.config.ts
+++ apps/nexus/eslint.config.ts
@@ -2,6 +2,15 @@ import type { Linter } from 'eslint';
 
 import { nextConfig } from '@auibsal/config/eslint.config';
 
-const config: Linter.Config[] = [...nextConfig];
+const config: Linter.Config[] = [
+  ...nextConfig,
+  {
+    // Local Nexus overrides
+    ignores: ['.next/**', 'node_modules/**'],
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+];
 
 export default config;
