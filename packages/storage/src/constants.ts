7196d164f5a3b6fddecd748a48cf6b12bb637107
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-05-31 05:43:59 +0300

    Create constants.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

---
+++ packages/storage/src/constants.ts
@@ -0,0 +1,7 @@
+export const BUCKETS = {
+  // Requires Row Level Security (RLS) and signed URLs to access
+  MANUSCRIPTS: 'manuscripts',
+  
+  // Open read access for the Next.js `<Image />` component
+  ASSETS: 'assets',
+} as const;
