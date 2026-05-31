ac3971d3c944cc584cb0442c716990e5d1e18251
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-06-01 02:27:58 +0300

    Create json-ld.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

---
+++ packages/seo/src/json-ld.ts
@@ -0,0 +1,34 @@
+/**
+ * Generates Google-compliant JSON-LD structured data for a published manuscript.
+ * This should be injected into the <head> of a reading page.
+ */
+export function generateArticleSchema({
+  title,
+  authorName,
+  publishedAt,
+  url,
+}: {
+  title: string;
+  authorName: string;
+  publishedAt: string; // ISO 8601 Date
+  url: string;
+}) {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: title,
+    author: {
+      '@type': 'Person',
+      name: authorName,
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: 'AUIB Society of Arts & Letters',
+    },
+    datePublished: publishedAt,
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': url,
+    },
+  };
+}
