83dfd3018cacf3f88566bee4b3e4b630232e4775
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-06-01 02:21:55 +0300

    Create vitest.config.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,14 @@
+import { mergeConfig, defineConfig } from 'vitest/config';
+import { sharedVitestConfig } from '@auibsal/testing/vitest';
+
+export default mergeConfig(
+  sharedVitestConfig,
+  defineConfig({
+    test: {
+      // 🚨 OVERRIDE: Boot the virtual browser instead of Node
+      environment: 'jsdom', 
+      // 🚨 OVERRIDE: Inject the visual testing vocabulary
+      setupFiles: ['./vitest.setup.ts'], 
+    },
+  })
+);
