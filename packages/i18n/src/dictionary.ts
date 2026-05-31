51306b6339c49094b5da4a155614981c487f6026
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-06-01 02:36:18 +0300

    Create dictionary.ts
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

---
+++ packages/i18n/src/dictionary.ts
@@ -0,0 +1,11 @@
+import type { Locale } from './types';
+import { defaultLocale } from './types';
+
+const loaders = {
+  en: () => import('./messages/en').then((module) => module.en),
+  ar: () => import('./messages/ar').then((module) => module.ar),
+};
+
+export const getDictionary = async (locale: Locale) => {
+  return loaders[locale]?.() ?? loaders[defaultLocale]();
+};
