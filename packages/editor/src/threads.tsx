d69a4072dcde7269d4b8ac1c16753e993bb52c56
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-06-02 12:51:36 +0300

    Create threads.tsx
    Signed-off-by: Shaheen K. Farjo <shaheen.farjo@gmail.com>

---
+++ packages/editor/src/threads.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { useThreads } from "@liveblocks/react/suspense";
+import { AnchoredThreads, FloatingComposer, FloatingThreads } from "@liveblocks/react-tiptap";
+import type { Editor } from "@tiptap/react";
+
+export function EditorThreads({ editor }: { editor: Editor | null }) {
+  // Fetch all unresolved comments for this manuscript
+  const { threads } = useThreads({ query: { resolved: false } });
+
+  if (!editor) return null;
+
+  return (
+    <>
+      {/* Desktop side-panel comments */}
+      <div className="hidden sm:block absolute top-0 right-[-320px] w-[300px]">
+        <AnchoredThreads editor={editor} threads={threads} />
+      </div>
+
+      {/* Mobile floating comments */}
+      <div className="block sm:hidden">
+        <FloatingThreads editor={editor} threads={threads} />
+      </div>
+
+      {/* The pop-up input to write a new comment */}
+      <FloatingComposer editor={editor} className="w-[300px]" />
+    </>
+  );
+}
